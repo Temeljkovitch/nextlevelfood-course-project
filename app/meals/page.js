d3cade8a4e7377c078565ec8fc2d6583bbfde055
Goran <temeljkovitch.goran@gmail.com>
2024-12-22 17:48:51 -0400

    feat: add better-sqlite3 for database functionality

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,14 +1,15 @@
-
 import styles from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import MealsHeader from "@/components/meals/meals-header";
+import { getMeals } from "@/db/meal";
 
 export default function Meals() {
+  const meals = getMeals();
   return (
     <>
       <MealsHeader />
       <main className={styles.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
